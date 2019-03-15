63b89b2d63aade2c47e588e6ad9eeb8b3c2b526a
Peckins <messias.morais@etec.sp.gov.br>
2019-03-15 16:38:23 -0300

    Inicio do Projeto

---
+++ Projeto Spa/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Projeto_Spa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Spa Boa Vida";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome do Cliente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+            this.label4.Location = new System.Drawing.Point(100, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Altura";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(157, 173);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(161, 20);
+            this.txtAltura.TabIndex = 4;
+            this.txtAltura.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(157, 131);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(161, 20);
+            this.txtPeso.TabIndex = 5;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(157, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(161, 20);
+            this.txtNome.TabIndex = 6;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnVerificar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+            this.btnVerificar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnVerificar.Location = new System.Drawing.Point(157, 212);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(161, 29);
+            this.btnVerificar.TabIndex = 7;
+            this.btnVerificar.Text = "Verificar Dieta";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+            this.btnSair.Location = new System.Drawing.Point(204, 254);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 29);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(462, 295);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Projeto Spa/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Spa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Estrutura de armazenamento de dados do cliente
+        public struct Cliente
+        {
+            public string nome;
+            public double peso;
+            public double altura;
+        }
+
+        //variáveis de checagem de entrada
+        double numero;
+
+        //Definindo altura
+        private void textBox1_TextChanged(object sender, EventArgs e)//Textbox de Definição de altura
+        {
+            double altura;
+            Cliente select;
+
+            if (double.TryParse(txtAltura.Text, out numero))//Verifica se o usuário digitou numero
+            {
+                MessageBox.Show("Digite um número válido!");
+            }
+            else
+            {
+                altura = double.Parse(txtAltura.Text);
+                select.altura = altura;
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Definindo o peso
+        private void txtPeso_TextChanged(object sender, EventArgs e)//Textbox de Peso
+        {
+            double peso;
+            Cliente select;
+
+            if (double.TryParse(txtPeso.Text, out numero))//Verifica se o usuário digitou número
+            {
+                MessageBox.Show("Digite um número válido");
+            }
+            else
+            {
+                peso = double.Parse(txtPeso.Text);
+                select.peso = peso;
+            }
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            string nome;
+            Cliente select;
+
+            if (double.TryParse(txtNome.Text, out numero))
+        }
+    }
+}
